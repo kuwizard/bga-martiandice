4d4f24a1e70374b585027a67d51c7d07141369cf
Pavel Kulagin <pavel.kulagin@corp.badoo.com>
2020-08-18 17:34:37 +0100

    fixed a bug with second-column logs

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -112,7 +112,7 @@ $gameinfos = array(
     //  default: null (ie: no limit, the game interface is as big as the player's screen allows it).
     //  maximum possible value: unlimited
     //  minimum possible value: 740
-    'max' => 787
+    'max' => null
 ),
 
 // Game presentation
